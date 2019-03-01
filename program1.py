23be7c6748543db055150e5000b0d7543263c4d0
Gabriele Mazzola <gabriele.mazzola@techedgegroup.com>
2019-03-01 14:01:00 +0100

    create aggregated network

--- program1.py
+++ program1.py
@@ -1,6 +1,21 @@
 import json
+import networkx as nx
 
 with open('network.json') as f:
-    data = json.load(f)
+    edges_list = json.load(f)
 
-print(data[1])
+print(f"Loading network with {len(edges_list)} timesteps")
+
+
+# Build aggregated network
+flat_list = [item for sublist in edges_list for item in sublist]
+tmp = list(map(list,zip(*flat_list)))
+unique_nodes = set(tmp[0] + tmp[1])
+
+G = nx.Graph()
+G.add_nodes_from(list(unique_nodes))
+G.add_edges_from([tuple(item) for sublist in edges_list for item in sublist])
+
+print(G.number_of_nodes())
+print(G.number_of_edges())
+
